3413b59980868da3898a749f8fb27cf872cdea3d
wdev-jahidhasan <wdev.jahidhasan@gmail.com>
2026-07-26 00:54:02 +0600

    loading spinner added

---
+++ src/app/my-added-cars/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center bg-slate-800">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600"></div>
+    </div>
+  );
+}
